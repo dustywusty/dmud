da3075731645e64206e6ce555df5df911b7c5c33
Dusty <dustyc@me.com>
2023-04-23 07:46:39 -0500

    organized internal and external methods together abc

--- internal/game/game.go
+++ internal/game/game.go
@@ -19,95 +19,15 @@ type PlayerCommand struct {
   command *Command
 }
 
-
-// Game initialization
 //
-
-func NewGame() *Game {
-  game := &Game{
-    clients:          make(map[string]Player),
-    addPlayerChan:    make(chan Player),
-    removePlayerChan: make(chan Player),
-    commandChan:      make(chan *PlayerCommand),
-    rooms:            make(map[int]*Room),
-  }
-
-  // Initialize rooms and connect them
-  room1 := NewRoom(1, "Room 1", "The room is white and nondescript, with claw marks on the ceiling that you can't help but notice. In one corner, there's a pile of Ayn Rand books.\n\nThe atmosphere is oppressive, and you feel a growing sense of unease.")
-  room2 := NewRoom(2, "Room 0", "It's dark and damp, and you can't see anything. You hear a faint dripping sound.\n\nYou feel a sense of dread, and you're not sure why.")
-
-	room1.AddExit("down", room2)
-  room2.AddExit("up", room1)
-
-  game.rooms[room1.ID] = room1
-  game.rooms[room2.ID] = room2
-
-  go game.loop()
-
-  return game
-}
-
-//
-// Game loop
-//
-
-// loop is the main event loop for the game, handling player additions, removals, and command execution.
-func (g *Game) loop() {
-  for {
-    select {
-    case player := <-g.addPlayerChan:
-      g.clients[player.RemoteAddr()] = player
-    case player := <-g.removePlayerChan:
-      delete(g.clients, player.RemoteAddr())
-    case playerCmd := <-g.commandChan:
-      g.executeCommand(playerCmd.player, playerCmd.command)
-    }
-  }
-}
-
-//
-// Helper functions
+// Public methods
 //
 
 func (g *Game) AddPlayer(player Player) {
   g.addPlayerChan <- player
-	startingRoom := g.rooms[1]
-	startingRoom.AddPlayer(player)
-	player.SendMessage(startingRoom.Description)
-}
-
-func (g *Game) RemovePlayer(player Player) {
-  g.removePlayerChan <- player
-	room, ok :=g.findPlayerRoom(player)
-	if !ok {
-		room.RemovePlayer(player)
-	}
-}
-
-func (g *Game) findPlayerRoom(player Player) (*Room, bool) {
-  for _, room := range g.rooms {
-    if _, ok := room.Players[player.RemoteAddr()]; ok {
-      return room, true
-    }
-  }
-  return nil, false
-}
-
-func (g *Game) movePlayerToRoom(player Player, room *Room) {
-	currentRoom, ok := g.findPlayerRoom(player)
-	if !ok {
-		log.Printf("Player %s is not in any room??", player.RemoteAddr())
-	}
-	currentRoom.RemovePlayer(player)
-
-	enterMessage := fmt.Sprintf("%s has entered the room.", player.Name())
-	for _, p := range currentRoom.Players {
-		p.SendMessage(enterMessage)
-	}
-
-	room.AddPlayer(player)
-
-	player.SendMessage(room.Description)
+  startingRoom := g.rooms[1]
+  startingRoom.AddPlayer(player)
+  player.SendMessage(startingRoom.Description)
 }
 
 func (g *Game) ExecuteCommand(player Player, cmd *Command) {
@@ -127,6 +47,63 @@ func (g *Game) ExecuteCommand(player Player, cmd *Command) {
   default:
     player.SendMessage("Huh?")
   }
+}
+
+func NewGame() *Game {
+  game := &Game{
+    clients:          make(map[string]Player),
+    addPlayerChan:    make(chan Player),
+    removePlayerChan: make(chan Player),
+    commandChan:      make(chan *PlayerCommand),
+    rooms:            make(map[int]*Room),
+  }
+
+  room1 := NewRoom(1, "Room 1", "The room is white and nondescript, with claw marks on the ceiling that you can't help but notice. In one corner, there's a pile of Ayn Rand books.\n\nThe atmosphere is oppressive, and you feel a growing sense of unease.")
+  room2 := NewRoom(2, "Room 0", "It's dark and damp, and you can't see anything. You hear a faint dripping sound.\n\nYou feel a sense of dread, and you're not sure why.")
+
+	room1.AddExit("down", room2)
+  room2.AddExit("up", room1)
+
+  game.rooms[room1.ID] = room1
+  game.rooms[room2.ID] = room2
+
+  go game.loop()
+
+  return game
+}
+
+func (g *Game) RemovePlayer(player Player) {
+  g.removePlayerChan <- player
+	room, ok :=g.findPlayerRoom(player)
+	if !ok {
+		room.RemovePlayer(player)
+	}
+}
+
+//
+// Private methods
+//
+
+func (g *Game) loop() {
+  for {
+    select {
+    case player := <-g.addPlayerChan:
+      g.clients[player.RemoteAddr()] = player
+    case player := <-g.removePlayerChan:
+      delete(g.clients, player.RemoteAddr())
+    case playerCmd := <-g.commandChan:
+      g.ExecuteCommand(playerCmd.player, playerCmd.command)
+    }
+  }
+}
+
+func (g *Game) findPlayerRoom(player Player) (*Room, bool) {
+  for _, room := range g.rooms {
+    if _, ok := room.Players[player.RemoteAddr()]; ok {
+      return room, true
+    }
+  }
+  return nil, false
 }
 
 func (g *Game) handleExit(player Player) {
@@ -195,30 +172,47 @@ func (g *Game) handleSay(player Player, cmd *Command) {
 }
 
 func (g *Game) handleScan(player Player) {
-  room, ok := g.findPlayerRoom(player)
-  if !ok {
-    player.SendMessage("Error: Cannot find your current room.")
-    return
-  }
+	room, ok := g.findPlayerRoom(player)
+	if !ok {
+		player.SendMessage("Error: Cannot find your current room.")
+		return
+	}
 
-  if len(room.Exits) == 0 {
-    player.SendMessage("There are no exits in this room.")
-  } else {
-    exits := make([]string, 0, len(room.Exits))
-    for direction := range room.Exits {
-      exits = append(exits, direction)
-    }
+	if len(room.Exits) == 0 {
+		player.SendMessage("There are no exits in this room.")
+	} else {
+		exits := make([]string, 0, len(room.Exits))
+		for direction := range room.Exits {
+			exits = append(exits, direction)
+	}
 
-		log.Printf("Room: %s (%d)", room.Name, room.ID)
+	log.Printf("Room: %s (%d)", room.Name, room.ID)
 
-    player.SendMessage(fmt.Sprintf("Exits: %s", strings.Join(exits, ", ")))
-  }
+	player.SendMessage(fmt.Sprintf("Exits: %s", strings.Join(exits, ", ")))
+	}
 }
 
 func (g *Game) handleShout(player Player, cmd *Command) {
-  message := strings.Join(cmd.Arguments, " ")
-  broadcastMessage := fmt.Sprintf("%s shouts: %s", player.Name(), message)
-  for _, p := range g.clients {
-    p.SendMessage(broadcastMessage)
-  }
+	message := strings.Join(cmd.Arguments, " ")
+	broadcastMessage := fmt.Sprintf("%s shouts: %s", player.Name(), message)
+	for _, p := range g.clients {
+		p.SendMessage(broadcastMessage)
+	}
+}
+
+func (g *Game) movePlayerToRoom(player Player, room *Room) {
+	currentRoom, ok := g.findPlayerRoom(player)
+	if !ok {
+		log.Printf("Player %s is not in any room??", player.RemoteAddr())
+	}
+	currentRoom.RemovePlayer(player)
+
+	enterMessage := fmt.Sprintf("%s has entered the room.", player.Name())
+	for _, p := range currentRoom.Players {
+		p.SendMessage(enterMessage)
+	}
+
+	room.AddPlayer(player)
+
+	player.SendMessage(room.Description)
 }
