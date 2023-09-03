cc66c4dde202bd16deb6dace4ec92346ab814c6c
Dusty <dustyc@me.com>
2023-09-03 04:44:39 -0600

    formatting

--- internal/components/health.go
+++ internal/components/health.go
@@ -4,20 +4,21 @@ import "sync"
 
 type HealthComponent struct {
 	sync.RWMutex
-	MaxHealth     int
-	CurrentHealth int
-}
 
-func (hc *HealthComponent) TakeDamage(amount int) {
-	hc.CurrentHealth -= amount
-	if hc.CurrentHealth < 0 {
-		hc.CurrentHealth = 0
-	}
+	CurrentHealth int
+	MaxHealth     int
 }
 
 func (hc *HealthComponent) Heal(amount int) {
 	hc.CurrentHealth += amount
 	if hc.CurrentHealth > hc.MaxHealth {
 		hc.CurrentHealth = hc.MaxHealth
+	}
+}
+
+func (hc *HealthComponent) TakeDamage(amount int) {
+	hc.CurrentHealth -= amount
+	if hc.CurrentHealth < 0 {
+		hc.CurrentHealth = 0
 	}
 }
