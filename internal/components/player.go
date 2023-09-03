e503b15fca4c053d8cb288d4234c52fce282fbff
Dusty <dustyc@me.com>
2023-09-03 04:44:24 -0600

    moving these into the game

--- internal/components/player.go
+++ internal/components/player.go
@@ -2,7 +2,6 @@ package components
 
 import (
 	"dmud/internal/common"
-	"strings"
 	"sync"
 
 	"github.com/rs/zerolog/log"
@@ -11,7 +10,6 @@ import (
 type PlayerComponent struct {
 	sync.RWMutex
 	Client common.Client
-	Health *HealthComponent
 	Name   string
 	Room   *RoomComponent
 	Target *PlayerComponent
@@ -58,54 +56,6 @@ func (p *PlayerComponent) Move(direction string) {
 	p.Room.AddPlayer(p)
 
 	p.Broadcast(p.Room.Description)
-}
-
-func (p *PlayerComponent) Say(msg string) {
-	p.Room.Broadcast(p.Name + " says: " + msg)
-}
-
-func (p *PlayerComponent) Scan() {
-	exits := []string{}
-	for _, exit := range p.Room.Exits {
-		exits = append(exits, exit.Direction)
-	}
-	p.Broadcast("Exits: " + strings.Join(exits, ", "))
-}
-
-func (p *PlayerComponent) Shout(msg string, depths ...int) {
-	if p.Room == nil {
-		p.Broadcast("You try to shout but it just comes out muffled.")
-		return
-	}
-	log.Info().Msgf("Shout: %s", msg)
-
-	depth := 10
-	if len(depths) > 0 {
-		depth = depths[0]
-	}
-
-	visited := make(map[*RoomComponent]bool)
-	queue := []*RoomComponent{p.Room}
-
-	for depth > 0 && len(queue) > 0 {
-		depth--
-		nextQueue := []*RoomComponent{}
-
-		for _, room := range queue {
-			visited[room] = true
-			for _, exit := range room.Exits {
-				if !visited[exit.Room] {
-					visited[exit.Room] = true
-					nextQueue = append(nextQueue, exit.Room)
-				}
-			}
-		}
-		queue = nextQueue
-	}
-
-	for room := range visited {
-		room.Broadcast(p.Name+" shouts: "+msg, p)
-	}
 }
 
 func (p *PlayerComponent) Whisper(target *PlayerComponent, msg string) {
