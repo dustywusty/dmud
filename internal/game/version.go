28daca53c02b162313164b10b73a56f7034d22d0
Dusty <dustyc@me.com>
2025-11-17 01:52:08 -0700

    just show the commit hash and url

--- internal/game/version.go
+++ internal/game/version.go
@@ -7,24 +7,14 @@ import (
 
 func handleVersion(player *components.Player, args []string, game *Game) {
 	gitCommit := version.GetVersion()
-	buildTime := version.GetBuildTime()
 	githubURL := version.GetGitHubURL()
 
 	var output string
 	output += "==============================================\n"
 	output += "               VERSION INFO                   \n"
 	output += "==============================================\n\n"
-
-	if gitCommit == "dev" {
-		output += "Version:    development build\n"
-		output += "Build time: " + buildTime + "\n"
-		output += "Repository: " + githubURL + "\n"
-	} else {
-		output += "Commit:     " + gitCommit + "\n"
-		output += "Build time: " + buildTime + "\n"
-		output += "View:       " + githubURL + "\n"
-	}
-
+	output += "Commit: " + gitCommit + "\n"
+	output += "View:   " + githubURL + "\n"
 	output += "\n==============================================\n"
 
 	player.Broadcast(output)
