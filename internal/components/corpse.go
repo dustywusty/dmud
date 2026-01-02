ba0c918a5d4fae16511aeaad972a4719f83501aa
Dusty <dustyc@me.com>
2026-01-02 04:33:15 -0700

    removing unhelpful comments and cleaning

--- internal/components/corpse.go
+++ internal/components/corpse.go
@@ -6,47 +6,28 @@ import (
 	"time"
 )
 
-// Corpse represents the remains of a dead entity
 type Corpse struct {
 	sync.RWMutex
 
-	VictimName string
-
-	VictimID common.EntityID
-
-	// Whether this was a player or NPC
-	WasPlayer bool
-
-	// When the corpse was created
+	VictimName  string
+	VictimID    common.EntityID
+	WasPlayer   bool
 	TimeOfDeath time.Time
-
-	// How long before the corpse decays (in seconds)
-	DecayTime time.Duration
-
-	Area *Area
-
-	// Items that can be looted from the corpse
-	Inventory *Inventory
-
-	// When the corpse was fully looted (empty)
-	LootedAt *time.Time
+	DecayTime   time.Duration // How long before the corpse decays (in seconds)
+	Area        *Area
+	Inventory   *Inventory
+	LootedAt    *time.Time
 }
 
-// IsDecayed checks if the corpse should be removed
 func (c *Corpse) IsDecayed() bool {
 	c.RLock()
 	defer c.RUnlock()
-
-	// If corpse was fully looted, decay after 5 seconds
-	if c.LootedAt != nil {
+	if c.LootedAt != nil { // If corpse was fully looted, decay after 5 seconds
 		return time.Since(*c.LootedAt) >= 5*time.Second
-	}
-
-	// Otherwise use normal decay time
+	} // Otherwise use normal decay time
 	return time.Since(c.TimeOfDeath) >= c.DecayTime
 }
 
-// MarkAsLooted marks the corpse as having been fully looted
 func (c *Corpse) MarkAsLooted() {
 	c.Lock()
 	defer c.Unlock()
@@ -54,11 +35,9 @@ func (c *Corpse) MarkAsLooted() {
 	c.LootedAt = &now
 }
 
-// GetDescription returns a description of the corpse
 func (c *Corpse) GetDescription() string {
 	c.RLock()
 	defer c.RUnlock()
-
 	if c.WasPlayer {
 		return "the corpse of " + c.VictimName
 	}
